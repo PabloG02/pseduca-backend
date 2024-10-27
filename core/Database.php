ba8a02c7f30806f2c7fbcb4f9ac9760fb8245c17
PabloG02 <devpablogg@gmail.com>
2024-10-27 17:31:10 +0100

    Add database connection management

---
+++ core/Database.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Core;
+
+use PDO;
+use PDOException;
+use RuntimeException;
+
+class Database
+{
+    private static ?PDO $connection;
+
+    public static function getConnection(): PDO
+    {
+        if (!isset(self::$connection)) {
+            try {
+                $host = 'localhost:3306';
+                $db = 'pseduca';
+                $user = 'root';
+                $pass = 'password';
+
+                $dsn = "mysql:host=$host;dbname=$db";
+                $options = [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                    PDO::ATTR_EMULATE_PREPARES => false, // TODO: Check if necessary
+                ];
+
+                self::$connection = new PDO($dsn, $user, $pass, $options);
+            } catch (PDOException $e) {
+                throw new RuntimeException('Database connection failed: ' . $e->getMessage());
+            }
+        }
+
+        return self::$connection;
+    }
+}
