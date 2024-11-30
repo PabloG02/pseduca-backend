a303e303f771c7b7b548d69216fd4237ebfcccdc
PabloG02 <devpablogg@gmail.com>
2024-11-30 17:42:08 +0100

    Set response Content-Type to JSON

--- index.php
+++ index.php
@@ -9,6 +9,9 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
+// Set the content type to JSON
+header('Content-Type: application/json');
+
 // Handle preflight requests
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
